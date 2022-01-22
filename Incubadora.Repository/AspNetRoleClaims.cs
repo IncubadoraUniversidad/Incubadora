1e4fb068e95a74981b1ee98ec8054df39a31c650
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-01-22 15:33:11 -0600

    Se modifico la clase roleclaims

--- Incubadora.Repository/AspNetRoleClaims.cs
+++ Incubadora.Repository/AspNetRoleClaims.cs
@@ -27,22 +27,4 @@ using System;
         public virtual AspNetRoles AspNetRoles { get; set; }
     }
 
-public partial class AspNetRoleClaims
-{
-
-    public int Id { get; set; }
-
-    public string RoleId { get; set; }
-
-    public string ClaimType { get; set; }
-
-    public string ClaimValue { get; set; }
-
-
-
-    public virtual AspNetRoles AspNetRoles { get; set; }
-
-}
-
-
 }
