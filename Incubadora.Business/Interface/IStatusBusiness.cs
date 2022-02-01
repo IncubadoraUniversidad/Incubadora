751f1cc79db909a3907f0f08de687498c2cc7f69
Daniel Falcon <juandanielperezfalcon1998@gmail.com>
2022-02-01 13:50:20 -0600

    Se agregaron todos los elementos relacionados con el status

---
+++ Incubadora.Business/Interface/IStatusBusiness.cs
@@ -0,0 +1,28 @@
+﻿using Incubadora.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incubadora.Business.Interface
+{
+    public interface IStatusBusiness
+    {
+        List<StatusDomainModel> GetStatus();
+
+
+        /// <summary>
+        /// Este metodo se encarga de agregar  actualziar un rol
+        /// </summary>
+        /// <param name="statusDM">la entidad AspNetRoles</param>
+        /// <returns>un valor boolean true/false</returns>
+        bool AddUpdateStatus(StatusDomainModel statusDM);
+        /// <summary>
+        /// Este metodo se encarga de validar que una entidad no exista 
+        /// </summary>
+        /// <param name="strValor">El parametro a buscar  </param>
+        /// <returns>Valor boleano true o false</returns>
+        bool ValidateStatusValue(string strValor);
+    }
+}
--- Incubadora/ViewModels/StatusVM.cs
+++ Incubadora/ViewModels/StatusVM.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace Incubadora.Domain
+namespace Incubadora.ViewModels
 {
-    public class StatusDomainModel
+    public class StatusVM
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string StrValor { get; set; }
         //public string StrDescripcion { get; set; }
     }
