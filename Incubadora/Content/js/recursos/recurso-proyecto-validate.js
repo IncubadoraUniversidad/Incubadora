b843307a7a86d236a68b1b9fa4a508b8c7d2f872
Oscar Reyes <reyesmartinezoscar3g15@gmail.com>
2022-03-19 13:57:13 -0600

    Database Initializer v1 terminado

---
+++ Incubadora/Content/js/recursos/recurso-proyecto-validate.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    $('#StrNombreRecurso').keyup(function () {
+        const value = $(this).val();
+        const parentElement = this.parentElement;
+        if (value.length === 0) {
+            parentElement.classList.add('has-error');
+            showInputError(parentElement.lastElementChild, 'Este campo es requerido');
+            return;
+        }
+        parentElement.classList.remove('has-error');
+        hideInputError(parentElement.lastElementChild);
+    });
+
+    const showInputError = (element, message) => {
+        const jqueryElement = $(element);
+        jqueryElement.text(message);
+        jqueryElement.show();
+    };
+
+    const hideInputError = (element) => {
+        $(element).hide();
+    };
+});
